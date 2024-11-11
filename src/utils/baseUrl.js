523391634884d548d42e72f47ca7d9056744f3ca
Williams <avendano.balarezo.williams@gmail.com>
2024-11-11 18:08:09 -0500

    changing url from utils/baseUrl

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000";
+    //return "http://localhost:5000";
+    return "https://full-stack-book-store-app-using-react-node-mongo-db-backend.vercel.app";
 }
 
 export default getBaseUrl;
